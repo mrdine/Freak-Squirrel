1ae92a5dc5a91fe3fa467da7cbd3eb052d525b39
Daniel <daniel.gomes071@gmail.com>
2019-04-23 23:36:55 -0300

    adicionado player e movimentaçao

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
+    private float vel = 5f;
+    private float fall = 0f;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        fall = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+
+        //Gravidade
+        transform.Translate(new Vector3(0, fall * Time.fixedDeltaTime, 0));
+        fall -= 0.02f;
+        //Entradas do teclado para controlar o personagem
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, vel * Time.fixedDeltaTime, 0));
+            fall += 1f;
+            vel -= 0.5f;
+            if(fall >= 0)
+            {
+                fall = 0;
+            }
+        }
+
+        if(fall <= 0.08f)
+        {
+            vel = 5f;
+        }
+    }
 }
