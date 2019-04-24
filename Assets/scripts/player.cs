a4075df456d81b6337df5acc5cffc067611d106b
Daniel <daniel.gomes071@gmail.com>
2019-04-24 11:56:35 -0300

    impossibilitando do player sair dos limites da camera e adicionado um inimigo

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -16,6 +16,17 @@ public class player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        var distanceZ = (transform.position - Camera.main.transform.position).z;
+
+        var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceZ)).x;
+
+        var righBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceZ)).x;
+
+        var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceZ)).y;
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distanceZ)).y;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftBorder, righBorder), Mathf.Clamp(transform.position.y, topBorder, bottomBorder), transform.position.z);
 
         //Gravidade
         transform.Translate(new Vector3(0, fall * Time.fixedDeltaTime, 0));
@@ -30,6 +41,7 @@ public class player : MonoBehaviour {
             {
                 fall = 0;
             }
+
         }
 
         if(fall <= 0.08f)
